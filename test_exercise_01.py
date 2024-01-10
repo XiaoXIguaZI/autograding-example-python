59c46edcc1edcb1ae2d80f9e81b5a544a35a6025
XiaoXIguaZI <155779459+XiaoXIguaZI@users.noreply.github.com>
2024-01-10 12:50:30 -0600

    Update test_exercise_01.py

--- test_exercise_01.py
+++ test_exercise_01.py
@@ -1,9 +1,5 @@
 import hello
 
 def test_hello():
-    """
-    Exercise 1.1:
-    Write a function `hello.hello_world`
-    that returns the string "Hello, World!"
-    """
+    hello.hello_world
     assert hello.hello_world() == "Hello, World!"
